b44209e76e88898fadbc4de79f52e64f9ea5dcae
nida <nidhfz85@gmail.com>
2026-06-29 22:06:11 +0500

    upd

--- client/src/Enquiry.jsx
+++ client/src/Enquiry.jsx
@@ -7,12 +7,20 @@ export default function Enquiry() {
         <h1 className='text-[40px] text-center py-6 font-bold'> User Enquiry</h1>
 
         <div className='grid grid-cols-[30%_auto]'>
-               <div className='bg-[#F5F5F5] h-[100vh]'>
+               <div className='bg-gray-200 p-4'>
                 <h2 className='text-[24px] font-bold py-4'>Enquiry form</h2>
                 <form>
                   <div className="py-3">
                     <Label htmlFor="name">Your Name</Label>
-                    <TextInput id="name" type="text" placeholder="John Doe" required />
+                    <TextInput id="name" type="text" placeholder="Enter your name" required />
+                  </div>
+                  <div className="py-3">
+                    <Label htmlFor="email">Your Email</Label>
+                    <TextInput id="email" type="email" placeholder="Enter your email" required />
+                  </div>
+                  <div className="py-3">
+                    <Label htmlFor="phone">Your Phone</Label>
+                    <TextInput id="phone" type="tel" placeholder="Enter your phone number" required />
                   </div>
                 </form>
                 </div>             
