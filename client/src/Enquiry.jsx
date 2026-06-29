3c265d7e9d64e5612652838e9a90724db89ef9a9
nida <nidhfz85@gmail.com>
2026-06-29 21:59:36 +0500

    upd

--- client/src/Enquiry.jsx
+++ client/src/Enquiry.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 export default function Enquiry() {
   return (
     <div>
         
-        <h1 className='text-[40px] text-center py-6'> User Enquiry</h1>
+        <h1 className='text-[40px] text-center py-6 font-bold'> User Enquiry</h1>
+
+        <div className='grid grid-cols-[30%_auto]'>
+               <div className='bg-[#F5F5F5] h-[100vh]'>
+                <h2 className='text-[24px] font-bold py-4'>Enquiry form</h2>
+                <form>
+                  <div className="py-3">
+<label>Name:</label>
+                  </div>
+                </form>
+                </div>             
+
+        </div>
         </div>
   )
 }
