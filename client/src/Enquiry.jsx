a4789d33e1bdbf50819032d412ec30f4f6f22f33
nida <nidhfz85@gmail.com>
2026-06-29 21:24:54 +0500

    upd

---
+++ client/src/Enquiry.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Enquiry() {
+  return (
+    <div>
+        
+        <h1 className='text-[40px] text-center py-6'> User Enquiry</h1>
+        </div>
+  )
+}
