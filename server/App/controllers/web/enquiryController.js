2202701cbf9000328be04b17bba018c4a65fd38a
nida <nidhfz85@gmail.com>
2026-07-07 13:12:10 +0500

    upd

---
+++ server/App/controllers/web/enquiryController.js
@@ -0,0 +1,6 @@
+let enquiryInsert=(req, res)=>{
+
+    res.send("Enquiry Inserted Successfully");
+
+}
+module.exports={enquiryInsert};
--- server/App/routes/web/EnquiryRoutes.js
+++ server/App/routes/web/EnquiryRoutes.js
@@ -1,4 +1,8 @@
 let express=require('express');
 let enquiryRouter=express.Router();
+const {enquiryInsert}=require('../../controllers/web/enquiryController');
+
+enquiryRouter.post('/insert',enquiryInsert); 
+
 
 module.exports=enquiryRouter;
